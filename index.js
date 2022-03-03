a11f60a1ec2199b41fc797824904066ad1795ff0
josephbill <josephbill00@gmail.com>
2022-03-03 17:37:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const viewedNo = new Set();
+  for (const no of array) {
+    const same = target - no;
+
+    if (viewedNo.has(same)) return true;
+
+    viewedNo.add(no);
+  }
+  return false;
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +24,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
 }
 
 module.exports = hasTargetSum;
